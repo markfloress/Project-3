e94fe00083215be92c5be6015740e23cd73a33ad
Mark <mark@Marks-MacBook-Pro.local>
2017-08-04 10:17:56 -0700

    added wall collision

--- src/partials/ball.js
+++ src/partials/ball.js
@@ -9,9 +9,24 @@ export default class ball{
     this.reset();
   }
 
+  wallCollision(){
+    const hitLeft = this.x - this.r <= 0                            //redundant remove later after paddle collision
+    const hitRight = this.x + this.r >= this.boardWidth
+    const hitTop = this.y - this.r <= 0
+    const hitBot = this.y + this.r >= this.boardHeight
+
+    if (hitLeft || hitRight){
+      this.vx = -this.vx
+    } else if (hitTop || hitBot){
+      this.vy = -this.vy
+    }
+  }
+
   render(svg) {
     this.x += this.vx
     this.y += this.vy
+
+    this.wallCollision();
     
     let ball = document.createElementNS(SVG_NS, 'circle');
     ball.setAttributeNS(null, 'r', this.r);
